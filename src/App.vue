f2d4bc5d5b868232430d2183ef8f19fc66a5097f
Dennis Meinen <bluepunk@gmail.com>
2021-02-16 11:52:36 +0100

    Start

--- src/App.vue
+++ src/App.vue
@@ -3,15 +3,18 @@
   <Grid 
     v-bind:width="9" 
     v-bind:height="9" />
+  <KillerCalculator />
 </template>
 
 <script>
 import Grid from './components/Grid.vue'
+import KillerCalculator from './components/KillerCalculator.vue'
 
 export default {
   name: 'Sudoku',
   components: {
-    Grid
+    Grid,
+    KillerCalculator
   }
 }
 </script>
---
+++ src/components/KillerCalculator.vue
@@ -0,0 +1,37 @@
+<template>
+    <div>
+        <h3>Calculator</h3>
+        <div>
+            <label><input v-model="sum" @change="onInputChange" placeholder="Sum" type="number" /></label>
+            <label><input v-model="cells" @change="onInputChange" placeholder="Cells" type="number" /></label>
+        </div>
+    </div>
+</template>
+
+<script>
+
+
+export default {
+    name: 'KillerCalculator',
+    methods: {
+        onInputChange() {
+            console.log(this.sum * this.cells)
+        },
+    },
+    data() {
+        return {
+            sum: 45,
+            cells: 9,
+        }
+    }
+
+}
+</script>
+
+<style scoped>
+    div {
+        border: 1px solid #f00;
+        position: absolute;
+        left: 65%;
+    }
+</style>
